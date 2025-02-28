2be48531ac2a1697916fa06c056266813ce9b7c1
Yanzhen Huang <huangyanzhen0108@163.com>
2025-02-28 16:10:19 +0800

    :bee: Add file type check.

--- CRUD/face/services.py
+++ CRUD/face/services.py
@@ -1,9 +1,13 @@
+# Basic
+import os
+import base64
+import magic
+
 # FastAPI server essentials
 from fastapi import APIRouter, Depends, HTTPException, status
 
 # PostgreSQL database connection
 from sqlalchemy.orm import Session
-
 
 # Locals
 from database import get_db
@@ -13,8 +17,9 @@ from CRUD.user.models import WRITE
 from CRUD.user.services import find_user_by
 from auth import validate_user
 
-
 router = APIRouter()
+
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 
 
 @router.post("/upload_face/")
@@ -25,9 +30,9 @@ def upload_face(face_upload: FaceUpload, db: Session = Depends(get_db)):
     :param db: Database session.
     :return: Upload message.
     """
+    # Check for user validation.
     uploader_id = face_upload.user_id
     uploader_token = face_upload.token
-
     validate_user(user_id=uploader_id, token=uploader_token)
 
     db_uploader = find_user_by(attr="id",
@@ -38,6 +43,20 @@ def upload_face(face_upload: FaceUpload, db: Session = Depends(get_db)):
     if not db_uploader.check_permission(permission=WRITE):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail=f"The user {uploader_id} does not have the permission to upload face.")
+
+    # Check for file types.
+    try:
+        file_data = base64.b64decode(face_upload.blob)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Decode base64 data error: Unable to decode.")
+
+    mime_type = magic.from_buffer(file_data, mime=True)
+    file_ext = mime_type.split("/")[-1]
+
+    if file_ext not in ALLOWED_EXTENSIONS:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Unsupported file type: {file_ext}.")
 
     new_face = Face(
         uploaded_by=face_upload.user_id,
