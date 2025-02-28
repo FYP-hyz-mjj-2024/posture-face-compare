1fae3c6c29d272fba44c28fc2cc662461ca0bacf
Yanzhen Huang <huangyanzhen0108@163.com>
2025-02-28 17:30:21 +0800

    :bee: Face detection and feature extraction with dlib.

--- CRUD/face/services.py
+++ CRUD/face/services.py
@@ -1,6 +1,11 @@
 # Basic
 import os
 import base64
+from io import BytesIO
+from PIL import Image
+import dlib
+import numpy as np
+import cv2
 import magic
 
 # FastAPI server essentials
@@ -20,6 +25,44 @@ from auth import validate_user
 router = APIRouter()
 
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
+
+face_detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(os.path.join(os.path.dirname(__file__),
+                                              "predictor",
+                                              "shape_predictor_68_face_landmarks.dat"))
+
+
+def retrieve_face_feature(blob):
+    """
+    Retrieve face features from a blob. If there's no face in the image, return none.
+    :param blob: Blob, base64 of an image.
+    :return: The face feature of the first discovered face in the image,
+             if this image contains any faces. Otherwise, None.
+    """
+
+    # Retrieve image file from base64.
+    file_data = base64.b64decode(blob)
+    _image = Image.open(BytesIO(file_data))
+
+    # Convert to dlib-readable format.
+    image = np.array(_image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Retrieve faces
+    faces = face_detector(image_gray)
+
+    if len(faces) == 0:
+        return None
+
+    # Use the most significant face.
+    face = faces[0]
+
+    # Face landmarks.
+    landmarks = predictor(image_gray, face)
+    face_feature = np.array([landmarks.part(i) for i in range(68)])
+
+    return face_feature
 
 
 @router.post("/upload_face/")
@@ -58,6 +101,13 @@ def upload_face(face_upload: FaceUpload, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"Unsupported file type: {file_ext}.")
 
+    face_feature = retrieve_face_feature(face_upload.blob)
+
+    if face_feature is None:
+        return {
+            "msg": "No face detected."
+        }
+
     new_face = Face(
         uploaded_by=face_upload.user_id,
         blob=face_upload.blob,
@@ -72,5 +122,6 @@ def upload_face(face_upload: FaceUpload, db: Session = Depends(get_db)):
         "face_id": new_face.id,
         "uploaded_at": new_face.uploaded_at,
         "uploaded_by": new_face.uploaded_by,
-        "description": new_face.description
+        "description": new_face.description,
+        "face_feature": str(face_feature)
     }
